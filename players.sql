7de98a2ade5a844d09e0c25b00f3026863dc6669
Rajee <rajee_natch@hotmail.com>
2022-01-06 14:19:50 -0500

    Create Table SQL script for players, and teams tables

---
+++ players.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS public.players
+(
+  player_id INT NOT NULL,
+  first_name VARCHAR ( 50 ) UNIQUE NOT NULL,
+  last_name VARCHAR ( 50 ) UNIQUE NOT NULL,
+  positions VARCHAR ( 50 ) UNIQUE NOT NULL,
+  team_id INT NOT NULL,
+  team_full_name VARCHAR ( 50 ) UNIQUE NOT NULL,
+  PRIMARY KEY (player_id),
+  FOREIGN KEY (team_id)
+      REFERENCES teams (team_id)
+);
+
+CREATE TABLE IF NOT EXISTS public.teams
+(
+  team_id INT NOT NULL,
+  team_full_name VARCHAR ( 50 ) UNIQUE NOT NULL,
+  PRIMARY KEY (team_id)
+  
+)
