776af17b4f28db791c0b58419b521a4663525805
Andrewsr220a <Raish.a.andrews@gmail.com>
2022-01-13 21:50:47 -0500

    plotly changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from datetime import datetime
-
+import plotly.express as px
 from flask import Flask, jsonify, render_template
 from config import DB_KEY
 import pandas as pd
@@ -19,30 +19,40 @@ import json
 connection_string = "postgres:Dontforget123!@localhost:5432/Basketball_stats"
 engine = create_engine(f'postgresql://{connection_string}')
 
-# reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(engine, reflect=True)
+games_table_df = pd.read_sql_query('select * from "seasons"', con=engine)
 
-# Save reference to the table
-# Players = Base.classes.players
-# Teams = Base.classes.teams
+# games_table = list(engine.execute("select * from games"))
 
+# games_table_df = pd.games_table
+# print(games_table_df)
 
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-teams_headings = ("index", "team_id", "team_full_name")
+# teams_headings = ("index", "team_id", "team_full_name")
 #################################################
 # Flask Routes
 #################################################
 
 
 @app.route("/")
+def index():
+    # teams_table = list(engine.execute("select * from seasons"))
+    season_df = pd.read_sql('select * from seasons', engine)
+    fig1 = px.line(season_df, x='Season', y='Win%', title='Teams Legacy')
+    graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("index.html", seasons=season_df, graph1JSON=graph1JSON)
+
+
+@app.route("/teams-data")
 def teams():
-    teams_table = list(engine.execute("select * from teams"))
-    return render_template("index.html", teams=teams_table)
+    # teams_table = list(engine.execute("select * from seasons"))
+    season_df = pd.read_sql('select * from seasons', engine)
+    fig1 = px.line(season_df, x='Season', y='Win%', title='Teams Legacy')
+    graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+    # return render_template("index.html", seasons=season_df, graph1JSON=graph1JSON)
+    return season_df.to_json(orient='records')
 
 
 @app.route("/players")
@@ -58,8 +68,5 @@ def games():
     return render_template("schedule_Page.html", games=games_table)
 
 
-df = pd.read_csv()
-df = df[df[] == ]
 if __name__ == '__main__':
     app.run(debug=True)
-++
